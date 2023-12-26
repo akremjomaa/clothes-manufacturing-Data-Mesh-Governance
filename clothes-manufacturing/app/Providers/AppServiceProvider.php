1da9370db9d9f95129b7c69cc735afc6da2496e5
akrem jomaa <jomaakrem@gmail.com>
2023-12-26 19:33:50 +0100

    rectifing

--- clothes-manufacturing/app/Providers/AppServiceProvider.php
+++ clothes-manufacturing/app/Providers/AppServiceProvider.php
@@ -11,16 +11,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        /*$consul = $this->app->make(Consul::class);
-
-        // Enregistrement du microservice dans Consul
-        $consul->register([
-            'name' => 'fabrication-microservice', // Nom du microservice
-            'address' => '127.0.0.1', // Adresse du microservice
-            'port' => 8081, // Port du microservice
-            'tags' => ['manufactoring'], // Balises pour le microservice
-
-        ]);*/
     }
 
     /**
